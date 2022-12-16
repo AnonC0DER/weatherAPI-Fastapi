eedce0d2dbedb8b2449ccb3246f9b5983e5ecb7c
Hesam Norin <hesam.norin@yahoo.com>
2022-12-16 21:12:57 +0330

    Bugs fixed

--- views/home.py
+++ views/home.py
@@ -1,15 +1,18 @@
 import fastapi
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
-from services import report_service
+from services.report_service import get_reports
 
 router = fastapi.APIRouter()
 templates = Jinja2Templates('templates')
 
 @router.get('/', include_in_schema=False)
 async def home(request: Request):
-    events = await report_service.get_reports()
-    return templates.TemplateResponse('home/home.html', {'request' : request, 'events' : events})
+    reports = get_reports()
+    events = reports['reports']
+    count = reports['count']
+
+    return templates.TemplateResponse('home/home.html', {'request' : request, 'events' : events, 'count' : count})
 
 
 @router.get('/favicon.ico', include_in_schema=False)
